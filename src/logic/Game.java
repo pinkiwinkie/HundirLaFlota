febc147d069dd5282575074b10c65cde6db7615d
pinkiwinkie <97902221+pinkiwinkie@users.noreply.github.com>
2022-11-25 22:18:26 +0100

    java doc finished and refactored too.

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -44,18 +44,19 @@ public class Game {
         int option;
         do {
             System.out.println("Hi, we are going to play to sink the fleet. You want to do?");
-            option = Input.getInt("1.Read the instructions.\n2.Play in easy mode.\n3.Play against AI.\n4.Exit.");
+//            option = Input.getInt("1.Read the instructions.\n2.Play in easy mode.\n3.Play against AI.\n4.Exit.");
+            option = Input.getInt("1.Play in easy mode.\n2.Exit.");
             switch (option){
-                case 1:
-                    tutorial();
+                case 4:
+//                    tutorial();
                     break;
-                case 2:
+                case 1:
                     gameplay();
                     break;
                 case 3:
 //                    jugabilidadConIa();
                     break;
-                case 4:
+                case 2:
                     System.out.println("Bye.");
                     exit = true;
                 default:
--- src/logic/Ships.java
+++ src/logic/Ships.java
@@ -35,13 +35,13 @@ public class Ships {
     }
 
     /**
-     * @param board
-     * @param shipLength
-     * @param row
-     * @param col
-     * @param shipOrientation
-     * @param player          so that the user has control when putting the boats.
-     * @return true if is there is a collision and false if is there is no collision.
+     * @param board      to see coordinate and ships.
+     * @param row        in the coordinate.
+     * @param col        in the coordinate.
+     * @param player     to show messages.
+     * @param shipLength to keep in mind that the restrictions are inside the dashboard.
+     * @return true if it is free and
+     * false if is not free.
      */
     public static boolean thereIsACollision(char[][] board, int shipLength, int row, int col, int shipOrientation, boolean player) {
         boolean collides;
@@ -55,37 +55,46 @@ public class Ships {
 
     }
 
-    private static boolean verticalCollision(char[][] board, int shipLength, int row, int columna, boolean jugador) {
+    /**
+     * @param board      to see coordinate and ships.
+     * @param row        in the coordinate.
+     * @param col        in the coordinate.
+     * @param player     to show messages.
+     * @param shipLength to keep in mind that the restrictions are inside the dashboard.
+     * @return true if it is free and
+     * false if is not free.
+     */
+    private static boolean verticalCollision(char[][] board, int shipLength, int row, int col, boolean player) {
         if (shipLength == 1) {
             if ((row + shipLength) <= board[0].length) {
-                if (board[row][columna] == 'B') {
-                    if (jugador)
+                if (board[row][col] == 'B') {
+                    if (player)
                         System.err.println("The cell is occupied.");
                     return true;
                 }
-                if (!alrededorVerticalVacio(board, row, columna, jugador, 0))
+                if (!alrededorVerticalVacio(board, row, col, player, 0))
                     return true;
-                if (!alrededorHorizontalVacio(board, row, columna, jugador, 0))
+                if (!alrededorHorizontalVacio(board, row, col, player, 0))
                     return true;
             } else {
-                if (jugador)
+                if (player)
                     System.err.println("The ship does not fit down.");
                 return true;
             }
         } else {
             for (int i = 0; i < shipLength; i++) {
                 if ((row + shipLength) <= board[0].length) {
-                    if (board[row + i][columna] == 'B') {
-                        if (jugador)
+                    if (board[row + i][col] == 'B') {
+                        if (player)
                             System.err.println("The cell is occupied.");
                         return true;
                     }
-                    if (!alrededorVerticalVacio(board, row, columna, jugador, i))
+                    if (!alrededorVerticalVacio(board, row, col, player, i))
                         return true;
-                    if (!alrededorHorizontalVacio(board, row, columna, jugador, i))
+                    if (!alrededorHorizontalVacio(board, row, col, player, i))
                         return true;
                 } else {
-                    if (jugador)
+                    if (player)
                         System.err.println("The ship does not fit down.");
                     return true;
                 }
@@ -94,6 +103,15 @@ public class Ships {
         return false;
     }
 
+    /**
+     * @param board      to see coordinate and ships.
+     * @param row        in the coordinate.
+     * @param col        in the coordinate.
+     * @param player     to show messages.
+     * @param shipLength to keep in mind that the restrictions are inside the dashboard.
+     * @return true if it is free and
+     * false if is not free.
+     */
     private static boolean horizontalCollision(char[][] board, int shipLength, int row, int col, boolean player) {
         if (shipLength == 1) {
             if (board[row][col] == 'B') {
@@ -127,68 +145,95 @@ public class Ships {
         return false;
     }
 
-    public static boolean alrededorVerticalVacio(char[][] board, int fila, int columna, boolean jugador, int i) {
+    /**
+     * @param board  to see coordinate and ships.
+     * @param row    in the coordinate.
+     * @param col    in the coordinate.
+     * @param player to show messages.
+     * @param i      ship length.
+     * @return true if it is free and
+     * false if is not free.
+     */
+    public static boolean alrededorVerticalVacio(char[][] board, int row, int col, boolean player, int i) {
         //comprobar esquina superior izquierda y esquina superior derecha. (A,1) y (A,9).
-        if ((fila == 1 && columna == 1) || (fila == 1 && columna == 9)) {
-            if (!isFreeNextRow(board, fila, columna, jugador, i))
+        if ((row == 1 && col == 1) || (row == 1 && col == 9)) {
+            if (!isFreeNextRow(board, row, col, player, i))
                 return false;
 
             //comprobar esquina inferior izquierda y esquina inferior derecha. (I,1) y (I,9).
-        } else if ((fila == 9 && columna == 1) || (fila == 9 && columna == 9)) {
-            if (!isFreePreviousRow(board, fila, columna, jugador, i))
+        } else if ((row == 9 && col == 1) || (row == 9 && col == 9)) {
+            if (!isFreePreviousRow(board, row, col, player, i))
                 return false;
-        } else if (fila == 9) {
-            if ((!isFreePreviousRow(board, fila, columna, jugador, i)) || (!esColumnaSiguienteLibre(board, fila, columna, jugador, i)))
+        } else if (row == 9) {
+            if ((!isFreePreviousRow(board, row, col, player, i)) || (!esColumnaSiguienteLibre(board, row, col, player, i)))
                 return false;
-        } else if (columna == 1) {
-            if ((!isFreeNextRow(board, fila, columna, jugador, i)) || (!isFreePreviousRow(board, fila, columna, jugador, i)))
+        } else if (col == 1) {
+            if ((!isFreeNextRow(board, row, col, player, i)) || (!isFreePreviousRow(board, row, col, player, i)))
                 return false;
-        } else if (columna == 9) {
-            if ((!isFreeNextRow(board, fila, columna, jugador, i)) || (!isFreePreviousRow(board, fila, columna, jugador, i)) || (!esColumnaAnteriorLibre(board, fila, columna, jugador, i)))
+        } else if (col == 9) {
+            if ((!isFreeNextRow(board, row, col, player, i)) || (!isFreePreviousRow(board, row, col, player, i)) || (!esColumnaAnteriorLibre(board, row, col, player, i)))
                 return false;
-        } else if (fila == 1) {
-            if ((!isFreeNextRow(board, fila, columna, jugador, i)) || (!esColumnaAnteriorLibreOColumnaSiguienteLibre(board, fila, columna, jugador, i)))
+        } else if (row == 1) {
+            if ((!isFreeNextRow(board, row, col, player, i)) || (!esColumnaAnteriorLibreOColumnaSiguienteLibre(board, row, col, player, i)))
                 return false;
         } else {
             //busca coincidencias en el interior del mapa.
-            if ((!esColumnaAnteriorLibreOColumnaSiguienteLibre(board, fila, columna, jugador, i)) || (!esFilaAnteriorLibreOFilaSiguienteLibre(board, fila, columna, jugador, i)))
+            if ((!esColumnaAnteriorLibreOColumnaSiguienteLibre(board, row, col, player, i)) || (!esFilaAnteriorLibreOFilaSiguienteLibre(board, row, col, player, i)))
                 return false;
         }
         return true;
     }
 
-    public static boolean alrededorHorizontalVacio(char[][] board, int fila, int columna, boolean jugador, int i) {
+    /**
+     * @param board  to see coordinate and ships
+     * @param row    in the coordinate
+     * @param col    in the coordinate
+     * @param player to show messages
+     * @param i      ship length
+     * @return true if it is free and
+     * false if is not free
+     */
+    public static boolean alrededorHorizontalVacio(char[][] board, int row, int col, boolean player, int i) {
         //comprobar esquina superior izquierda y esquina inferior izquierda. (A,1) y (I,1).
-        if ((fila == 1 && columna == 1) || (fila == 9 && columna == 1)) {
-            if (!esColumnaSiguienteLibre(board, fila, columna, jugador, i))
+        if ((row == 1 && col == 1) || (row == 9 && col == 1)) {
+            if (!esColumnaSiguienteLibre(board, row, col, player, i))
                 return false;
             //comprobar esquina superior derecha y esquina inferior izquierda. (A,9) y (I,9).
-        } else if ((fila == 1 && columna == 9) || (fila == 9 && columna == 9)) {
-            if (!esColumnaAnteriorLibre(board, fila, columna, jugador, i))
+        } else if ((row == 1 && col == 9) || (row == 9 && col == 9)) {
+            if (!esColumnaAnteriorLibre(board, row, col, player, i))
                 return false;
             //comprobar que no salga del board haciendo la busqueda en la posición contigua.
-        } else if (columna == 9) {
-            if ((!esColumnaAnteriorLibre(board, fila, columna, jugador, i)) || (!isFreePreviousRow(board, fila, columna, jugador, i)))
+        } else if (col == 9) {
+            if ((!esColumnaAnteriorLibre(board, row, col, player, i)) || (!isFreePreviousRow(board, row, col, player, i)))
                 return false;
 //comprueba que haciendo la búsqueda no se salga por arriba ni por la izquierda del board.
-        } else if ((fila == 1) || (columna == 1)) {
-            if ((!esColumnaSiguienteLibre(board, fila, columna, jugador, i)) || (!esColumnaAnteriorLibre(board, fila, columna, jugador, i)) || (!isFreeNextRow(board, fila, columna, jugador, i)))
+        } else if ((row == 1) || (col == 1)) {
+            if ((!esColumnaSiguienteLibre(board, row, col, player, i)) || (!esColumnaAnteriorLibre(board, row, col, player, i)) || (!isFreeNextRow(board, row, col, player, i)))
                 return false;
-        } else if (fila == 9) {
-            if ((!isFreePreviousRow(board, fila, columna, jugador, i)) || (!esColumnaSiguienteLibre(board, fila, columna, jugador, i)) || (!esColumnaAnteriorLibre(board, fila, columna, jugador, i)) || (!isFreeNextRow(board, fila, columna, jugador, i)))
+        } else if (row == 9) {
+            if ((!isFreePreviousRow(board, row, col, player, i)) || (!esColumnaSiguienteLibre(board, row, col, player, i)) || (!esColumnaAnteriorLibre(board, row, col, player, i)))
                 return false;
 //busca coincidencias en el interior del mapa
         } else {
-            if (((!esFilaAnteriorLibreOFilaSiguienteLibre(board, fila, columna, jugador, i))) || (!esColumnaSiguienteLibre(board, fila, columna, jugador, i)))
+            if (((!esFilaAnteriorLibreOFilaSiguienteLibre(board, row, col, player, i))) || (!esColumnaSiguienteLibre(board, row, col, player, i)))
                 return false;
         }
         return true;
     }
 
-    private static boolean esColumnaAnteriorLibreOColumnaSiguienteLibre(char[][] board, int fila, int columna, boolean jugador, int i) {
-        if (fila + i < 10) {
-            if ((board[fila + i][columna - 1] == 'B') || (board[fila + i][columna + 1] == 'B')) {
-                if (jugador)
+    /**
+     * @param board  to see coordinate and ships
+     * @param row    in the coordinate
+     * @param col    in the coordinate
+     * @param player to show messages
+     * @param i      ship length
+     * @return true if it is free and
+     * false if is not free
+     */
+    private static boolean esColumnaAnteriorLibreOColumnaSiguienteLibre(char[][] board, int row, int col, boolean player, int i) {
+        if (row + i < 10) {
+            if ((board[row + i][col - 1] == 'B') || (board[row + i][col + 1] == 'B')) {
+                if (player)
                     System.err.println("You can not put a ship attached to another.");
                 return false;
             }
@@ -196,10 +241,19 @@ public class Ships {
         return true;
     }
 
-    private static boolean esColumnaSiguienteLibre(char[][] board, int fila, int columna, boolean jugador, int i) {
-        if (columna + (i + 1) < 10) {
-            if (board[fila][columna + (i + 1)] == 'B') {
-                if (jugador)
+    /**
+     * @param board  to see coordinate and ships
+     * @param row    in the coordinate
+     * @param col    in the coordinate
+     * @param player to show messages
+     * @param i      ship length
+     * @return true if it is free and
+     * false if is not free
+     */
+    private static boolean esColumnaSiguienteLibre(char[][] board, int row, int col, boolean player, int i) {
+        if (col + (i + 1) < 10) {
+            if (board[row][col + (i + 1)] == 'B') {
+                if (player)
                     System.err.println("You can not put a ship attached to another.");
                 return false;
             }
@@ -207,10 +261,19 @@ public class Ships {
         return true;
     }
 
-    private static boolean esColumnaAnteriorLibre(char[][] board, int fila, int columna, boolean jugador, int i) {
-        if (fila + i < 10) {
-            if (board[fila + i][columna - 1] == 'B') {
-                if (jugador)
+    /**
+     * @param board  to see coordinate and ships.
+     * @param row    in the coordinate.
+     * @param col    in the coordinate.
+     * @param player to show messages.
+     * @param i      ship length.
+     * @return true if it is free and
+     * false if is not free.
+     */
+    private static boolean esColumnaAnteriorLibre(char[][] board, int row, int col, boolean player, int i) {
+        if (row + i < 10) {
+            if (board[row ][col - 1] == 'B') {
+                if (player)
                     System.err.println("You can not put a ship attached to another.");
                 return false;
             }
@@ -218,10 +281,19 @@ public class Ships {
         return true;
     }
 
-    private static boolean esFilaAnteriorLibreOFilaSiguienteLibre(char[][] board, int fila, int columna, boolean jugador, int i) {
-        if (columna + i < 10) {
-            if ((board[fila - 1][columna + i] == 'B') || (board[fila + 1][columna + i] == 'B')) {
-                if (jugador)
+    /**
+     * @param board  to see coordinate and ships.
+     * @param row    in the coordinate.
+     * @param col    in the coordinate.
+     * @param player to show messages.
+     * @param i      ship length.
+     * @return true if it is free and
+     * false if is not free.
+     */
+    private static boolean esFilaAnteriorLibreOFilaSiguienteLibre(char[][] board, int row, int col, boolean player, int i) {
+        if (col + i < 10) {
+            if ((board[row - 1][col + i] == 'B') || (board[row + 1][col + i] == 'B')) {
+                if (player)
                     System.err.println("You can not put a ship attached to another.");
                 return false;
 
@@ -231,18 +303,18 @@ public class Ships {
     }
 
     /**
-     * @param board   to see coordinate and ships.
-     * @param fila    in the coordinate.
-     * @param columna in the coordinate.
-     * @param jugador to show messages.
-     * @param i       ship length.
+     * @param board  to see coordinate and ships.
+     * @param row    in the coordinate.
+     * @param col    in the coordinate.
+     * @param player to show messages.
+     * @param i      ship length.
      * @return true if it is free and
      * false if is not free.
      */
-    private static boolean isFreePreviousRow(char[][] board, int fila, int columna, boolean jugador, int i) {
+    private static boolean isFreePreviousRow(char[][] board, int row, int col, boolean player, int i) {
 
-        if (board[fila - 1][columna] == 'B') {
-            if (jugador)
+        if (board[row - 1][col] == 'B') {
+            if (player)
                 System.err.println("You can not put a ship attached to another.");
             return false;
 
